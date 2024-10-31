af56ede501964a9aa9c32effb6154fce33315f56
massimiliano_arosio_abstract <massimiliano.arosio@abstract.it>
2024-10-31 15:03:10 +0100

    feat(05): added Link component

--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,15 @@
 // our-domanain.com/news
 
+import Link from "next/link";
+
 export default function Newspage() {
-    return <h1>The News page</h1>
+    return (
+        <>
+            <h1>The News page</h1>
+            <ul>
+                <li><Link href='/news/nextjs-is-a-great-framework'>NextJS is a great framework</Link></li>
+                <li>Something else</li>
+            </ul>
+        </>
+    )
 }
