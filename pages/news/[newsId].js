5c44b3cf332af4affe25fb39382c99c0ffb8ad5c
massimiliano_arosio_abstract <massimiliano.arosio@abstract.it>
2024-10-31 14:47:56 +0100

    feat(04): replace with dynamic path and routing

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,5 +1,11 @@
 // our-domanain.com/news/something-important
 
+import { useRouter } from "next/router"
+
 export default function Detailsgpage() {
+    const router = useRouter()
+
+    const newsID = router.query.newsId
+
     return <h1>The Details page</h1>
 }
